011e919e7559d5302bce6c32fb993515583b477f
Sheckerol <checol70@gmail.com>
2018-07-02 17:11:43 -0700

    Initial, only base files made

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+class Holder
+{
+    constructor(question, a0, a1, a2, a3, rA)
+    {
+        this.question = question;
+        this.answers=[a0,a1,a2,a3];
+        this.rightAns = rA;
+    }
+    TestAnswer()
+    {
+        
+    }
+} 
